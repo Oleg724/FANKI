3f974459d8325dc7a8fa025381c2a03381256b36
Oleg <7241984@gmail.com>
2020-09-29 18:56:54 +0300

    Updates

---
+++ precache-manifest.ede59fdf86657169b22c3295ed20cb3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c7248f41188d0cec06e084f9c9306a",
+    "url": "/FANKI-at-the-development-stage-/index.html"
+  },
+  {
+    "revision": "97049aa44614fd700811",
+    "url": "/FANKI-at-the-development-stage-/static/css/main.a6743c57.chunk.css"
+  },
+  {
+    "revision": "05e09d6c21dd20109e04",
+    "url": "/FANKI-at-the-development-stage-/static/js/2.e9487cf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FANKI-at-the-development-stage-/static/js/2.e9487cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97049aa44614fd700811",
+    "url": "/FANKI-at-the-development-stage-/static/js/main.0bd461b6.chunk.js"
+  },
+  {
+    "revision": "f6c21f2b63bfeb95dc3e",
+    "url": "/FANKI-at-the-development-stage-/static/js/runtime-main.b8e2d708.js"
+  }
+]);
